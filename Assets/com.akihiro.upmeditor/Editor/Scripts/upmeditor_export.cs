63d108db93e7ddcd9d52af3f48559786d063f35a
akihiro <ueyama0105@gmail.com>
2021-10-21 21:28:02 +0900

    add export

--- Assets/com.akihiro.upmeditor/Editor/Scripts/upmeditor_export.cs
+++ Assets/com.akihiro.upmeditor/Editor/Scripts/upmeditor_export.cs
@@ -1,6 +1,8 @@
 #if UNITY_EDITOR
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,11 +10,42 @@ namespace com.akihiro.upmeditor.editor
 {
     public class upmeditor_export : EditorWindow
     {
+        private string inputText = "";
 
         private void OnGUI()
         {
+            GUILayout.Label("UPM Export", EditorStyles.boldLabel);
 
+            inputText = EditorGUILayout.TextField(inputText);
+
+            if (GUILayout.Button("Init package"))
+            {
+                var path = Application.dataPath + "/"+ inputText;
+                Directory.CreateDirectory(path);
+                File.WriteAllText(path + "/README.md", "# "+ inputText);
+                File.WriteAllText(path + "/LICENSE", "");
+                var package = new PackageJson();
+                package.name = inputText;
+                package.version = "1.0.0";
+                package.displayName = inputText;
+                package.description = inputText;
+                package.unity = "2020.3";
+                var data=JsonUtility.ToJson(package, true);
+                File.WriteAllText(path + "/package.json", data);
+
+                AssetDatabase.Refresh();
+            }
         }
+    }
+
+    [Serializable]
+    public class PackageJson
+    {
+        public string name;
+        public string version;
+        public string displayName;
+        public string description;
+        public string unity;
     }
 }
 #endif
--- Assets/com.akihiro.upmeditor/Editor/Scripts/upmeditor_import.cs
+++ Assets/com.akihiro.upmeditor/Editor/Scripts/upmeditor_import.cs
@@ -3,68 +3,102 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Unity.Plastic.Newtonsoft.Json;
 using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 namespace com.akihiro.upmeditor.editor
 {
     public class upmeditor_import : EditorWindow
     {
+        private AddRequest addRequest;
+        private RemoveRequest removeReauest;
+        private ListRequest listRequest;
+        private PackageCollection packages;
+
         private void OnGUI()
         {
-            var manifest = Application.dataPath + "/../Packages/manifest.json";
             GUILayout.Label("UPM Import", EditorStyles.boldLabel);
-            if (GUILayout.Button("Open manifest.json"))
-            {
-                EditorUtility.OpenWithDefaultApp(manifest);
-            }
 
-            if (GUILayout.Button("Reload"))
-            {
-                GetManifestJson(manifest);
-            }
+            if (GUILayout.Button("Open manifest.json")) EditorUtility.OpenWithDefaultApp(Application.dataPath + "/../Packages/manifest.json");
 
+            if (GUILayout.Button("Add"))
+            {
+                AddManifest("git+ssh://git@github.com/akihiro0105/nanoKontrol2_Unity.git?path=/Assets/com.akihiro.nanokontrol2.sdk");
+            }
+            if (GUILayout.Button("Remove"))
+            {
+                RemoveManifest("com.akihiro.nanokontrol2.sdk");
+            }
+            if (GUILayout.Button("List"))
+            {
+                ListManifest();
+            }
         }
 
-        private void GetManifestJson(string manifest)
+        private void AddManifest(string identifier)
         {
-            var json = File.ReadAllText(manifest);
-            if (json != null)
+            addRequest=Client.Add(identifier);
+            EditorApplication.update += AddProgress;
+        }
+
+        private void AddProgress()
+        {
+            if (addRequest.IsCompleted)
             {
-                var manifestJson = JsonConvert.DeserializeObject<ManifestJson>(json);
-                var key = "com.akihiro.nanokontrol2.sdk";
-                if (manifestJson.dependencies.ContainsKey(key))
-                {
-                    manifestJson.dependencies.Remove(key);
-                }
-                else
-                {
-                    manifestJson.dependencies.Add(key, "git+ssh://git@github.com/akihiro0105/nanoKontrol2_Unity.git?path=/Assets/com.akihiro.nanokontrol2.sdk");
-                }
-                var data = JsonConvert.SerializeObject(manifestJson, Formatting.Indented);
-                File.WriteAllText(manifest, data);
+                if (addRequest.Status == StatusCode.Success)
+                    Debug.Log("Installed: " + addRequest.Result.packageId);
+                else if (addRequest.Status >= StatusCode.Failure)
+                    Debug.Log(addRequest.Error.message);
+
+                EditorApplication.update -= AddProgress;
             }
         }
 
+        private void RemoveManifest(string packageName)
+        {
+            removeReauest = Client.Remove(packageName);
+            EditorApplication.update += RemoveProgress;
+        }
 
-    }
+        private void RemoveProgress()
+        {
+            if (removeReauest.IsCompleted)
+            {
+                if (removeReauest.Status == StatusCode.Success)
+                    Debug.Log("Removed: " + removeReauest.PackageIdOrName);
+                else if (removeReauest.Status >= StatusCode.Failure)
+                    Debug.Log(removeReauest.Error.message);
 
-    #region Manifest.json
-    [Serializable]
-    public class ManifestJson
-    {
-        public List<Scopedregistry> scopedRegistries = new List<Scopedregistry>();
-        public Dictionary<string, string> dependencies = new Dictionary<string, string>();
-    }
+                EditorApplication.update -= AddProgress;
+            }
+        }
 
-    [Serializable]
-    public class Scopedregistry
-    {
-        public string name;
-        public string url;
-        public List<string> scopes = new List<string>();
+        private void ListManifest()
+        {
+            listRequest = Client.List();
+            EditorApplication.update += ListProgress;
+        }
+
+        private void ListProgress()
+        {
+            if (listRequest.IsCompleted)
+            {
+                if (listRequest.Status == StatusCode.Success)
+                {
+                    packages = listRequest.Result;
+                    foreach (var package in packages)
+                        Debug.Log("Package name: " + package.name);
+                }
+                else if (listRequest.Status >= StatusCode.Failure)
+                    Debug.Log(listRequest.Error.message);
+
+                EditorApplication.update -= ListProgress;
+            }
+        }
     }
-    #endregion
 }
 #endif
